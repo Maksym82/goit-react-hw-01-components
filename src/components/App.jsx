01f7ec228a6f2f5ddb21a8ad1d1acc785cd8e538
Maksym <112901851+Maksym82@users.noreply.github.com>
2023-07-24 17:33:22 +0300

    fix Task3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,8 @@ import { Profile } from './Profile/profile'
 import user from './Profile/user.json'
 import { Statistics } from './Statistics/statistics'
 import data from './Statistics/data.json'
-// import { FriendList } from './friendlist/friends'
-// import friends from './friendlist/friends.json'
+import { FriendList } from './FriendList/allfriends'
+import friends from './FriendList/friends.json'
 
 export const App = () => {
   return (
@@ -16,7 +16,7 @@ export const App = () => {
         fontSize: 20,
         color: '#010101'
       }}
-    >
+      >
       <Profile
         username={user.username}
         tag={user.tag}
@@ -26,9 +26,8 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
-
       <Statistics title="Upload stats" data={data} />
-      {/* <FriendList friends={friends} /> */}
+      <FriendList friends = {friends}  />
     </div>
   );
 };
